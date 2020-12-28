86d54738b100546165ad12926cddbddb864ae41f
stonewm <stone.wangmin@qq.com>
2020-12-28 18:35:21 +0800

    2020/12/28

--- database programming/python-using-msaccess-db/test_sqlalchemy.py
+++ database programming/python-using-msaccess-db/test_sqlalchemy.py
@@ -10,39 +10,39 @@ session = Session()
 
 class TestSqlalchemy(unittest.TestCase):
 
-    def test_query(self):
-        employees = session.query(Employee).all()
-        for emp in employees:
-            print(emp)
+    # def test_query(self):
+    #     employees = session.query(Employee).all()
+    #     for emp in employees:
+    #         print(emp)
 
     def test_query_one(self):
         employees = session.query(Employee).filter(Employee.first_name=="Ted").all()
         for emp in employees:
             print(emp)
 
-    def test_insert(self):
-        new_emp = Employee(
-            emp_id =9001,
-            first_name = "Alice",
-            last_name = "Brown"
-        )
-        session.add(new_emp)
-        session.commit()
-        session.close()
+    # def test_insert(self):
+    #     new_emp = Employee(
+    #         emp_id =9001,
+    #         first_name = "Alice",
+    #         last_name = "Brown"
+    #     )
+    #     session.add(new_emp)
+    #     session.commit()
+    #     session.close()
 
-    def test_update(self):
-        emp = session.query(Employee).get(9001)
-        if emp is not None:
-            emp.age = 20
-            session.commit()
-            session.close()
+    # def test_update(self):
+    #     emp = session.query(Employee).get(9001)
+    #     if emp is not None:
+    #         emp.age = 20
+    #         session.commit()
+    #         session.close()
 
-    def test_delete(self):
-         emp = session.query(Employee).get(9001)
-         if emp is not None:
-            session.delete(emp)
-            session.commit()
-            session.close()
+    # def test_delete(self):
+    #      emp = session.query(Employee).get(9001)
+    #      if emp is not None:
+    #         session.delete(emp)
+    #         session.commit()
+    #         session.close()
 
 
 if __name__ == "__main__":
